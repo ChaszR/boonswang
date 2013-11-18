3ee2fb3220a070aeb84a8ec608bec8c54bc9e9b7
Chasz Rainsford <contact@chaszrainsford.com>
2013-11-18 12:01:35 -0500

    Wordpress now detects environment and connects to the appropriate database accordingly.

--- wp-config.php
+++ wp-config.php
@@ -16,23 +16,34 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp-boonswang');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
+//define('DB_NAME', 'wp-boonswang');
+//
+///** MySQL database username */
+//define('DB_USER', 'root');
+//
+///** MySQL database password */
+//define('DB_PASSWORD', 'root');
+//
+///** MySQL hostname */
+//define('DB_HOST', 'localhost');
+//
+///** Database Charset to use in creating database tables. */
+//define('DB_CHARSET', 'utf8');
+//
+///** The Database Collate type. Don't change this if in doubt. */
+//define('DB_COLLATE', '');
+if (isset($_SERVER['PLATFORM']) && $_SERVER['PLATFORM'] == 'PAGODABOX') {
+    define('DB_NAME', $_SERVER['DB1_NAME']);
+    define('DB_USER', $_SERVER['DB1_USER']);
+    define('DB_PASSWORD', $_SERVER['DB1_PASS']);
+    define ('DB_HOST', $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT']);
+}
+else {
+    define('DB_NAME', 'your_database_name');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_HOST', 'localhost');
+}
 /**#@+
  * Authentication Unique Keys and Salts.
  *
